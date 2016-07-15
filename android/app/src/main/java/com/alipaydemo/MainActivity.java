62190f629f858eccf319952a63fe85176175f979
zuizui <huangzuizui@gmail.com>
2016-07-15 10:51:40 +0800

    init project

---
+++ android/app/src/main/java/com/alipaydemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.alipaydemo;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.alipay.RNAlipayPackage;  // <- import
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "alipayDemo";
+    }
+
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(), //<- Add comma
+            new RNAlipayPackage(this) //<- Add package
+        );
+    }
+}
